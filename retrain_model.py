97142687d91fa3783f47b50ebc0e61f1d3e5622a
Inbal <inbal.shalev@birdsonmars.com>
2024-09-07 14:09:57 +0200

    try run

--- retrain_model.py
+++ retrain_model.py
@@ -5,9 +5,6 @@ import json
 
 database = os.getenv('DATABASE_FULL_PATH')
 
-# def update_last_training_date_in_db():
-current_version = len(os.listdir('mlruns/models/fraud_analysis')) -1
-
 #get dates list: 
 sql_handler = TrainDatesHandler()
 if sql_handler.dates_df.shape[0] < current_version:
@@ -22,6 +19,8 @@ else:
     eval_results = data_prep_pipeline.re_train_pipeline(date=date)#, model_version=current_version+1)    
     print(json.dumps(eval_results))
 
+# def update_last_training_date_in_db():
+current_version = len(os.listdir('mlruns/models/fraud_analysis')) -1
     
 # def train_new_data(session=session):
 #     date = session['last_train_date']
